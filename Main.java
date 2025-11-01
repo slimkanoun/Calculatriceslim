dac6e304eb260e23a1c8d931acbac6af22f77ae0
Slim Kanoun <slim.kanoun@enis.tn>
2025-11-01 21:47:59 +0100

    Multiplication Soustraction Addition Main pour tester

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String... args) {
 	int a = 2;
 	int b = 3;
-	System.out.println("a * b = " + Operation.multiplication(a,b));
+	System.out.println(a + " * " + b + " = " + Operation.multiplication(a,b));
+	System.out.println(a + " - " + b + " = " + Operation.soustraction(a,b));
+	System.out.println(a + " + " + b + " = " + Operation.addition(a,b));
     }
 }
--- Operation.java
+++ Operation.java
@@ -1,7 +1,12 @@
 public class Operation {
     public static int multiplication(int a, int b) {
-	System.out.println("bonjour");
 	return(a * b);
+    }
+    public static int soustraction(int a, int b) {
+	return(a - b);
+    }
+    public static int addition(int a, int b) {
+	return(a + b);
     }
 }
     
