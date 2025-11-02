af0468577d839909f67eff152f176b9437d60c4c
Slim Kanoun <slim.kanoun@enis.tn>
2025-11-02 17:51:06 +0100

    Verification de la division par 0

--- Operation.java
+++ Operation.java
@@ -9,7 +9,11 @@ public class Operation {
 	return(a + b);
     }
      public static int division(int a, int b) {
-	return(a / b);
+	 if (b != 0) {  
+	     return(a / b);
+	 } else {
+	     return 0;
+	 }
     }
 }
     
